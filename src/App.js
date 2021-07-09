369fb4f7fb5677f2152e133dd9daa6cf16d69f57
Zach Painter <z.painter45@gmail.com>
2021-07-09 14:58:40 -0500

    added API call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Config from "./Config/config";
 
-function App() {
-  return (
-    <div>
-      <h1>Run Check hello world!</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  getWeather = () => {
+    fetch(`${Config.weather_URL}?key=${Config.API_Key}&q=Wilmington`)
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  render() {
+    return (
+      <>
+        <div>hello world!</div>
+        <div>
+          <button onClick={this.getWeather}>Get your weather</button>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
